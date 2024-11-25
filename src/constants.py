1660e4b305ef525105740527961ca72e032c75c2
ChandanHans <chandanhans2003@gmail.com>
2024-11-25 20:40:11 +0530

    template changed

--- src/constants.py
+++ src/constants.py
@@ -9,7 +9,7 @@ load_dotenv(dotenv_path=resource_path(".env"))
 
 GPT_KEY = os.environ["GPT_KEY"]
 CREDS_JSON = json.loads(os.environ["CREDS_JSON"])
-LETTER_TEMPLATE = resource_path("templates/latter_template.docx")
+LETTER_TEMPLATE = resource_path("templates/letter_template.docx")
 IMAGE_SHEET_ID = '1e4GzXCftJYFRbh3xKWnvRK8zE-FjOUut7FinhFj-2ug'
 ANNUAIRE_HERITIERS_SHEET_ID = "1W0CIhX7QPwP-zYQfu3cnHbZxwsb3MVqZwVrgAelS0iU"
 DEATH_CERTIFICATES_FOLDER_ID = '16r80-Mq5jDo6Lj9svu0hD7ULYMyyUnHp'
